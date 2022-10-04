88147b03aeff92de12685533e950a41c92edb998
Andyatama <andyatama69@gmail.com>
2022-10-04 12:23:46 +0700

    Bagi Bagi Lagi

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -13,7 +13,7 @@
                 <img src="https::/github.com/adrians-23.png" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Achmad Setiawan</a>
+                <a href="#" class="d-block">{{Auth()->user()->name}}</a>
             </div>
         </div>
 
